56ba5d6ea99c59b08866ab833e594c85e9826fc2
Simon Grätzer <simon@graetzer.org>
2015-07-09 12:19:15 +0200

    extracted audioplayer

---
+++ app/src/main/jni/audioplayer.c
@@ -0,0 +1,196 @@
+/*
+ * audioplayer.c: Encapsulate the OpenSL interface into something simpler to use
+ *
+ * (C) Copyright 2015 Simon Grätzer
+ * Email: simon@graetzer.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
+ */
+
+// engine interfaces
+ static SLObjectItf engineObject = NULL;
+ static SLEngineItf engineEngine;
+
+ // output mix interfaces
+ static SLObjectItf outputMixObject = NULL;
+ static SLEnvironmentalReverbItf outputMixEnvironmentalReverb = NULL;
+
+ // buffer queue player interfaces
+ static SLObjectItf bqPlayerObject = NULL;
+ static SLPlayItf bqPlayerPlay;
+ static SLAndroidSimpleBufferQueueItf bqPlayerBufferQueue;
+ static SLEffectSendItf bqPlayerEffectSend;
+ static SLVolumeItf bqPlayerVolume;
+
+
+ // create the engine and output mix objects
+ static void audioplayer_createEngine() {
+     // create engine
+     SLresult result = slCreateEngine(&engineObject, 0, NULL, 0, NULL, NULL);
+     assert(SL_RESULT_SUCCESS == result);
+
+     // realize the engine
+     result = (*engineObject)->Realize(engineObject, SL_BOOLEAN_FALSE);
+     assert(SL_RESULT_SUCCESS == result);
+
+     // get the engine interface, which is needed in order to create other objects
+     result = (*engineObject)->GetInterface(engineObject, SL_IID_ENGINE, &engineEngine);
+     assert(SL_RESULT_SUCCESS == result);
+
+     // create output mix, with environmental reverb specified as a non-required interface
+     const SLInterfaceID ids[1] = {SL_IID_ENVIRONMENTALREVERB};
+     const SLboolean req[1] = {SL_BOOLEAN_FALSE};
+     result = (*engineEngine)->CreateOutputMix(engineEngine, &outputMixObject, 1, ids, req);
+     assert(SL_RESULT_SUCCESS == result);
+
+     // realize the output mix
+     result = (*outputMixObject)->Realize(outputMixObject, SL_BOOLEAN_FALSE);
+     assert(SL_RESULT_SUCCESS == result);
+
+     // get the environmental reverb interface
+     // this could fail if the environmental reverb effect is not available,
+     // either because the feature is not present, excessive CPU load, or
+     // the required MODIFY_AUDIO_SETTINGS permission was not requested and granted
+     result = (*outputMixObject)->GetInterface(outputMixObject, SL_IID_ENVIRONMENTALREVERB,
+             &outputMixEnvironmentalReverb);
+     if (SL_RESULT_SUCCESS == result) {
+         result = (*outputMixEnvironmentalReverb)->SetEnvironmentalReverbProperties(
+                 outputMixEnvironmentalReverb, &reverbSettings);
+         (void)result;
+     }
+     // ignore unsuccessful result codes for environmental reverb, as it is optional for this example
+ }
+
+ // this callback handler is called every time a buffer finishes playing
+static void _bqPlayerCallback(SLAndroidSimpleBufferQueueItf bq, void *context) {
+     assert(bq == bqPlayerBufferQueue);
+     assert(NULL == context);
+     // for streaming playback, replace this test by logic to find and fill the next buffer
+     /*if (--nextCount > 0 && NULL != nextBuffer && 0 != nextSize) {
+         SLresult result;
+         // enqueue another buffer
+         result = (*bqPlayerBufferQueue)->Enqueue(bqPlayerBufferQueue, nextBuffer, nextSize);
+         // the most likely other result is SL_RESULT_BUFFER_INSUFFICIENT,
+         // which for this code example would indicate a programming error
+         assert(SL_RESULT_SUCCESS == result);
+     }*/
+
+    int16_t *buf_ptr = temp_buffer + global_bufsize * current_temp_buffer_ix;
+
+    ssize_t frameCount = audio_utils_fifo_read(sample_fifo, buf_ptr, global_bufsize);
+    if (frameCount > 0) {
+    SLresult result = (*bqPlayerBufferQueue)->Enqueue(bqPlayerBufferQueue, buf_ptr, frameCount * 2);
+        assert(SL_RESULT_SUCCESS == result);
+        current_temp_buffer_ix = (current_temp_buffer_ix + 1) % N_BUFFERS;
+    }
+ }
+
+ // create buffer queue audio player
+ static void createBufferQueueAudioPlayer() {
+     SLresult result;
+
+     // configure audio source
+     SLDataLocator_AndroidSimpleBufferQueue loc_bufq = {SL_DATALOCATOR_ANDROIDSIMPLEBUFFERQUEUE, 2};
+     SLDataFormat_PCM format_pcm = {
+         	.formatType = SL_DATAFORMAT_PCM,
+         	.numChannels = 1,
+         	.samplesPerSec = global_sample_rate * 1000,// Milli Hz
+         	.bitsPerSample = SL_PCMSAMPLEFORMAT_FIXED_16,
+         	.containerSize = SL_PCMSAMPLEFORMAT_FIXED_16,
+         	.channelMask = SL_SPEAKER_FRONT_CENTER,
+         	.endianness = SL_BYTEORDER_LITTLEENDIAN// TODO: compute real endianness
+       	};
+     SLDataSource audioSrc = {&loc_bufq, &format_pcm};
+
+     // configure audio sink
+     SLDataLocator_OutputMix loc_outmix = {SL_DATALOCATOR_OUTPUTMIX, outputMixObject};
+     SLDataSink audioSnk = {&loc_outmix, NULL};
+
+     // create audio player
+     const SLInterfaceID ids[3] = {SL_IID_BUFFERQUEUE, SL_IID_EFFECTSEND,
+             /*SL_IID_MUTESOLO,*/ SL_IID_VOLUME};
+     const SLboolean req[3] = {SL_BOOLEAN_TRUE, SL_BOOLEAN_TRUE,
+             /*SL_BOOLEAN_TRUE,*/ SL_BOOLEAN_TRUE};
+     result = (*engineEngine)->CreateAudioPlayer(engineEngine, &bqPlayerObject, &audioSrc, &audioSnk,
+             3, ids, req);
+     assert(SL_RESULT_SUCCESS == result);
+
+     // realize the player
+     result = (*bqPlayerObject)->Realize(bqPlayerObject, SL_BOOLEAN_FALSE);
+     assert(SL_RESULT_SUCCESS == result);
+
+     // get the play interface
+     result = (*bqPlayerObject)->GetInterface(bqPlayerObject, SL_IID_PLAY, &bqPlayerPlay);
+     assert(SL_RESULT_SUCCESS == result);
+
+     // get the buffer queue interface
+     result = (*bqPlayerObject)->GetInterface(bqPlayerObject, SL_IID_BUFFERQUEUE, &bqPlayerBufferQueue);
+     assert(SL_RESULT_SUCCESS == result);
+
+     // register callback on the buffer queue
+     result = (*bqPlayerBufferQueue)->RegisterCallback(bqPlayerBufferQueue,_ bqPlayerCallback, NULL);
+     assert(SL_RESULT_SUCCESS == result);
+
+     // get the effect send interface
+     result = (*bqPlayerObject)->GetInterface(bqPlayerObject, SL_IID_EFFECTSEND,
+             &bqPlayerEffectSend);
+     assert(SL_RESULT_SUCCESS == result);
+
+     // get the volume interface
+     result = (*bqPlayerObject)->GetInterface(bqPlayerObject, SL_IID_VOLUME, &bqPlayerVolume);
+     assert(SL_RESULT_SUCCESS == result);
+ }
+
+ void audioplayer_init(void *buffer) {
+
+ }
+
+ void audioplayer_startPlayback() {
+ // TODO put the init code somewhere else
+    // 2 bytes per sample
+    //sample_fifo_buffer = calloc(global_bufsize, sizeof(uint16_t));
+    //assert(sample_fifo_buffer);
+    //audio_utils_fifo_init(&sample_fifo, global_bufsize, 2, sample_fifo_buffer);
+
+    // set the player's state to playing
+    SLresult result = (*bqPlayerPlay)->SetPlayState(bqPlayerPlay, SL_PLAYSTATE_PLAYING);
+    assert(SL_RESULT_SUCCESS == result);
+ }
+
+ void audioplayer_stopPlayback() {
+    SLresult result = (*bqPlayerPlay)->SetPlayState(bqPlayerPlay, SL_PLAYSTATE_STOPPED);
+    assert(SL_RESULT_SUCCESS == result);
+
+    //audio_utils_fifo_deinit(&sample_fifo);
+    //free(sample_fifo_buffer);
+ }
+
+ // shut down the native audio system
+ void audioplayer_cleanup() {
+     // destroy buffer queue audio player object, and invalidate all associated interfaces
+     if (bqPlayerObject != NULL) {
+         (*bqPlayerObject)->Destroy(bqPlayerObject);
+         bqPlayerObject = NULL;
+         bqPlayerPlay = NULL;
+         bqPlayerBufferQueue = NULL;
+         bqPlayerEffectSend = NULL;
+         bqPlayerVolume = NULL;
+     }
+
+     // destroy output mix object, and invalidate all associated interfaces
+     if (outputMixObject != NULL) {
+         (*outputMixObject)->Destroy(outputMixObject);
+         outputMixObject = NULL;
+         outputMixEnvironmentalReverb = NULL;
+     }
+
+     // destroy engine object, and invalidate all associated interfaces
+     if (engineObject != NULL) {
+         (*engineObject)->Destroy(engineObject);
+         engineObject = NULL;
+         engineEngine = NULL;
+     }
+ }
---
+++ app/src/main/jni/audioplayer.h
@@ -0,0 +1,17 @@
+/*
+ * audioplayer.h: Encapsulate the OpenSL interface into something simpler to use
+ *
+ * (C) Copyright 2015 Simon Grätzer
+ * Email: simon@graetzer.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
+ */
+
+// Initialize the OpenSL audio interface
+void audioplayer_init(void *buffer);
+void audioplayer_startPlayback();
+void audioplayer_stopPlayback();
+void audioplayer_cleanup();
